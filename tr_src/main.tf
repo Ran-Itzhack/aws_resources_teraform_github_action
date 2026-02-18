9899f0a7c12b9d879f21a2ebc99325af54964e41
Ran Itzhack <ran.itzhack@gmail.com>
2026-02-18 12:06:02 -0800

    MOdify variable

--- tr_src/main.tf
+++ tr_src/main.tf
@@ -19,8 +19,8 @@ terraform {
 provider "aws" {
   alias      = "ohio"
   region     = "us-east-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  # access_key = var.access_key
+  # secret_key = var.secret_key
 }
 
 
